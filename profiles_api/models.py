d14c993b83df865c58525602f33339a8e902d86b
akhil <akhilgupta0698@gmail.com>
2021-05-27 00:38:25 +0530

    database configure

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,30 @@
+from pickle import NONE
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self,email,name,password=None):
+        """"create a new user profile"""
+        if not email:
+            raise ValueError("must have have a email")
+        email=self.normalize_email(email)
+        user =self.model(email=email,name=name)
+        user.set_password(password)#to keep password encrypted
+        user.save(using=self.db)# to make the sure database is attached
+
+        return user
+    def create_superuser(self,email,name,password):
+        """"create and save new super user with given details"""
+
+        user=self.create_user(email,name,password)
+        user.is_superuser=True  # created by permission mixin
+        user.is_staff=(True)    # created by permission mixin
+        user.save(using=self.db)
+        return user
+
+
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """"Databse model for user in the system """
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import profile
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'profile_api',
+    'profiles_api',
 
 ]
 
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL="profiles_api.UserProfile"
