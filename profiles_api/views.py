990cb15a1674acbdd371a4db69cf53f2d6733f54
akhil <akhilgupta0698@gmail.com>
2021-05-29 01:07:30 +0530

    viewset addition
    added item to viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from profiles_api import serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -42,5 +43,58 @@ class HelloApiView(APIView):
          return Response({'method': 'DELETE'})        
     # put updates the object fully
     # put updates the object partially, means update the object partially only thr required part is changed
-             
 
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    """Test API ViewSet"""
+    serializer_class=serializer.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message."""
+        ...
+
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
