1a3c115a9e80b7175b2a2d6622ad252f4923dd60
akhil <akhilgupta0698@gmail.com>
2021-06-02 00:53:56 +0530

    login api
    adding login feature

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 #from rest_framework import serializers
 
 class HelloApiView(APIView):
@@ -112,3 +114,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
